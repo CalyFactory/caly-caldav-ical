7220ddec94621994d6ee0575aa9d24a6c2e23b0b
refgjin <refgjin@gmail.com>
2017-01-25 17:59:02 +0900

    Dictionarize All Calendar

--- caly_caldav_ical.py
+++ caly_caldav_ical.py
@@ -4,7 +4,7 @@ import xml.sax
 import xml.dom.minidom
 import static
 from xml.etree.ElementTree import *
-
+import pprint
 """
 prin_tree = ElementTree(fromstring(response.text)).getroot()
 for child in prin_tree[0][1][0][0][0]:
@@ -22,6 +22,7 @@ class CaldavClient:
 
         self.userId = id
         self.userPw = pw 
+        self.calDict = {}
     
     def requestPROPFIND(self, host_url, req_data, req_depth=0):
         
@@ -47,7 +48,7 @@ class CaldavClient:
 
         prin_tree = ElementTree(fromstring(res.text)).getroot()
         self.prin_url=prin_tree[0][1][0][0][0].text
-        print(prin_tree[0][1][0][0][0].text)
+        #print(prin_tree[0][1][0][0][0].text)
 
         
     def getTopCalendarID(self):
@@ -69,51 +70,51 @@ class CaldavClient:
         all_cal_tree = ElementTree(fromstring(res.text)).getroot()
         #print(res.status_code)
         #print(res.status_code)
-        print(res.text)
+        #print(res.text)
         self.printAllCalendarName(all_cal_tree)
         #self.cal_url = "https://p62-caldav.icloud.com:443/10761962064/calendars/home/"
 
-    def printAllCalendarName(self,trees):
-        
+    def printAllCalendarName(self,trees):    
+        org_url, dummy = self.top_cal_url.split(":443/")
+        current_url=None
         for tree in trees:
             for child in tree:
                 if child.tag == "{DAV:}href":
-                    print("HREF", child.text)
+                    current_url=org_url+child.text
                     
                 if child.tag == "{DAV:}propstat":
                     #print("PROPSTAT", child[0][0].tag, child[0][0].attrib)#, len(child[0]))
-                    if "{DAV:}displayname" in child[0][0].tag:
-                        print("DS ",child[0][0].text)
+                    #if "{DAV:}displayname" in child[0][0].tag:
+                    #    print("DS ",child[0][0].text)
                     if len(child[0]) > 1:
                         #print("PROPSTAT", child[0][1].tag, child[0][1].attrib)
                         if "{http://calendarserver.org/ns/}getctag" in child[0][1].tag:
-                            print("GC ",child[0][1].text)
+                            display_name = child[0][0].text
+                            c_tag = child[0][1].text
+                            self.filterCal(current_url,display_name, c_tag, 1)
+                            
                     if len(child[0]) > 2:
                         #print("PROPSTAT", child[0][2].tag, child[0][2].attrib)
                         if "{http://calendarserver.org/ns/}getctag" in child[0][2].tag:
-                            print("GC ",child[0][2].text)
+                            display_name = child[0][0].text
+                            c_tag = child[0][2].text
+                            self.filterCal(current_url,display_name, c_tag, 2)
 
-                    print()
-                    
+        pp = pprint.PrettyPrinter(width=41, compact=True)
+        pp.pprint(self.calDict)
 
-    def getCtagCurrentCalendar(self):
-        data = (
-                "<?xml version=\"1.0\" encoding=\"utf-8\"?> "
-                "<d:propfind xmlns:d=\"DAV:\" xmlns:cs=\"http://calendarserver.org/ns/\"> "
-                "    <d:prop> "
-                "        <d:displayname /> "
-                "        <cs:getctag /> "
-                "    </d:prop> "
-                "</d:propfind>"
-            )
-        res = self.requestPROPFIND(self.cal_url,data,1)
+    def filterCal (self, current_url, display_name, c_tag, where):
+        #print("where",where)
+        if(display_name is None or c_tag is None):
+            return
 
-        ctag_tree = ElementTree(fromstring(res.text)).getroot()
-        #print(res.status_code)
-        #print(res.text)
-        """
-        for tree in ctag_tree:
-            for child in tree:
-                print(child.tag, child.attrib)
-                """
-        #self.printAllCalendarName(ctag_tree)
+        if(type(display_name) == "<class 'str'>" and type(display_name) == "<class 'str'>"):
+            display_name = display_name.replace(" ","")
+            c_tag = c_tag.replace(" ","")
+        
+        if(display_name is not "" and (c_tag is not "" or c_tag is not "None")):
+            #print("DS ",display_name)
+            #print("GC ",c_tag, "[0][1]")
+            self.calDict[current_url]=[display_name,c_tag]            
+            #print(self.calDict)
+
